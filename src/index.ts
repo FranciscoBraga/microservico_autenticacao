937025c6e2967e44487507ea5b97804e3b148222
FranciscoBraga <franciscobraga.fob@gmail>
2024-07-29 10:49:07 -0400

    Incrementeao o Typescript e incluindo script dev para

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Olá Mundo")
+import express, {Request,Response,NextFunction} from "express"
+
+const app =  express();
+
+//app.get chama uma função como callback
+app.get('/status',(req:Request,res:Response,next:NextFunction)=>{
+    res.status(200).send({foo:'Olá mundo!!'})
+
+}); //resolver
+
+//fica escultando na porta 3000
+app.listen(3000,()=>{
+    console.log("Aplicação funcionando na porta 3000!")
+})
